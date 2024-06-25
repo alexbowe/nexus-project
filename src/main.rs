d32a637daa66a20e2a77be8b9afe230565678835
Alex Bowe <alex@alexbowe.com>
2024-06-24 22:03:04 -0700

    Add basic RSA implementation (#1)
    * implement basic RSA
    
    * basic rsa implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 #![no_std]
 #![no_main]
 
-use nexus_rt::write_log;
+use nexus_rt::{
+    print,
+    Write,
+};
+
+use nexus_project::rsa::generate_rsa_key;
 
 #[nexus_rt::main]
 fn main() {
-    write_log("Hello, World!\n");
+    let rsa_key = generate_rsa_key();
+
+    let message = 42;
+    let encrypted = rsa_key.encrypt(message);
+    let decrypted = rsa_key.decrypt(encrypted);
+    assert_eq!(message, decrypted);
+
+    print!("{} -> {} -> {}\n", message, encrypted, decrypted);
 }
