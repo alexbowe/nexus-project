e96ee6714566d7a55e9f567f348ef7e7a18bc4e6
alexbowe <alex@alexbowe.com>
2024-06-27 22:01:38 -0700

    save game

--- src/main.rs
+++ src/main.rs
@@ -3,66 +3,27 @@
 
 use nexus_rt::{print, Write};
 
-use nexus_project::rsa::generate_rsa_key;
+use nexus_project::rsa::{BigUint, RsaKey};
 
-use crypto_bigint::{
-    modular::runtime_mod::{DynResidue, DynResidueParams},
-    Encoding, NonZero, U1024, U2048, U256,
-};
+use rand_core::SeedableRng;
 
-use rand_chacha::{
-    rand_core::{RngCore, SeedableRng},
-    ChaChaRng,
-};
-
-// use core::arch::asm;
-// fn read_seed_register() -> u32 {
-//     let mut seed: u32;
-//     unsafe {
-//         asm!("csrrw {}, seed, x0", out(reg) seed);
-//     }
-//     seed
-// }
+// TODO: Change to ChaCha20Rng for more rounds
+use rand_chacha::ChaCha8Rng;
 
 #[nexus_rt::main]
 fn main() {
-    let rsa_key = generate_rsa_key();
+    // TODO: Get entropy for seed from private input tape,
+    const ENTROPY_SIZE: usize = 32; // 256 bits = 32 bytes
+    let entropy = [0u8; ENTROPY_SIZE];
+    let mut rng = ChaCha8Rng::from_seed(entropy);
 
-    let message = 42;
+    let rsa_key = RsaKey::new(&mut rng);
+
+    // TODO: Add encode() function to encode strings
+    let message = BigUint::from_u64(42);
     let encrypted = rsa_key.encrypt(message);
     let decrypted = rsa_key.decrypt(encrypted);
-    assert_eq!(message, decrypted);
+    // assert_eq!(message, decrypted);
 
     print!("{} -> {} -> {}\n", message, encrypted, decrypted);
-
-    // TODO: Insecure. Need good entropy source (riscv asm?)
-    // Use seed register to get 16 bits of entropy at a time,
-    // combining them using sha to get 256 bits of entropy.
-    let entropy = [0u8; 32];
-    let mut rng: ChaChaRng = ChaChaRng::from_seed(entropy);
-    let mut buf = [0u8; 128];
-
-    rng.fill_bytes(&mut buf[..]);
-    let a = U1024::from_be_bytes(buf);
-    print!("a: {}\n", a);
-
-    rng.fill_bytes(&mut buf[..]);
-    let b = U1024::from_be_bytes(buf);
-    print!("b: {}\n", b);
-
-    let product: U2048 = a * b;
-    print!("product: {}\n", product);
-
-    // Define the base, exponent, and modulus
-    let base = U256::from_u64(4);
-    let exponent = U256::from_u64(13);
-    let modulus = U256::from_u64(497);
-
-    let non_zero_modulus = NonZero::new(modulus).unwrap();
-    let params = DynResidueParams::new(&non_zero_modulus);
-    let residue_base = DynResidue::new(&base, params);
-    let result = residue_base.pow(&exponent);
-    let result_u256 = result.retrieve();
-
-    print!("Result: {}\n", result_u256);
 }
--- src/rsa.rs
+++ src/rsa.rs
@@ -1,53 +1,120 @@
-use crate::math::mod_pow;
+use core::ops::AddAssign;
 
-// TODO: Implement basic BigUint, or try to get crypto-bigint working without std
+use nexus_rt::{print, Write};
+
+use crypto_bigint::{
+    modular::runtime_mod::{DynResidue, DynResidueParams},
+    Checked, CheckedSub, NonZero,
+};
+
+pub use crypto_bigint::{Random, Uint, U128, U256};
+use crypto_primes::generate_prime_with_rng;
+
+use rand_core::CryptoRngCore;
+
+// Should use at least 4096 bits in production.
+// Consider making RsaKey struct generic to
+// support user specified widths.
+const BITWIDTH: usize = 64;
+const LIMBS: usize = BITWIDTH / 32;
+const HALF_LIMBS: usize = LIMBS / 2;
+
+pub type BigUint = Uint<LIMBS>;
+type HalfSizeBigUint = Uint<HALF_LIMBS>;
+
 pub struct RsaKey {
-    e: u64,
-    d: u64,
-    // TODO: support public-only keys using Option<u64>
-    n: u64,
+    // TODO: support public-only keys using Option<>
+    e: BigUint,
+    d: BigUint,
+    n: BigUint,
 }
 
 impl RsaKey {
-    // pub fn new() -> RsaKey {
-    //     generate_rsa_key()
-    // }
+    pub fn new(rng: &mut impl CryptoRngCore) -> RsaKey {
+        generate_rsa_key(rng)
+    }
 
-    pub fn public_key(&self) -> (u64, u64) {
+    pub fn public_key(&self) -> (BigUint, BigUint) {
         (self.e, self.n)
     }
 
-    pub fn private_key(&self) -> (u64, u64) {
+    pub fn private_key(&self) -> (BigUint, BigUint) {
         (self.d, self.n)
     }
 
-    pub fn encrypt(&self, message: u64) -> u64 {
+    pub fn encrypt(&self, message: BigUint) -> BigUint {
         let (exponent, modulus) = self.public_key();
         mod_pow(message, exponent, modulus)
     }
 
-    pub fn decrypt(&self, ciphertext: u64) -> u64 {
+    pub fn decrypt(&self, ciphertext: BigUint) -> BigUint {
         let (exponent, modulus) = self.private_key();
         mod_pow(ciphertext, exponent, modulus)
     }
 }
 
-pub fn generate_rsa_key() -> RsaKey {
-    let p: u64 = 61;
-    let q: u64 = 53;
-    let n: u64 = p * q;
+fn generate_rsa_key<R: CryptoRngCore>(rng: &mut R) -> RsaKey {
+    //let p: HalfSizeBigUint = HalfSizeBigUint::from_u64(61);
+    //let q: HalfSizeBigUint = HalfSizeBigUint::from_u64(53);
+    let p = generate_random_prime(rng);
+    let q = generate_random_prime(rng);
+    let n = p * q;
 
-    // Euler's totient function φ(n)
-    let phi: u64 = (p - 1) * (q - 1);
+    let phi = totient(p, q);
 
     // Choose e such that 1 < e < φ(n) and gcd(e, φ(n)) = 1
-    // 65537 is a common choice, but too large for our small primes
-    let e: u64 = 17;
+    // 65537 is a common choice, but lets check our assumptions
+    let e = BigUint::from_u64(65537);
+    assert!(BigUint::ONE < e && e < phi); // 17 for p=61, q=53
+    assert!(gcd(e, phi) == BigUint::ONE);
 
-    let mut d: u64 = 2;
-    while (d * e) % phi != 1 {
-        d += 1;
-    }
+    let d = mod_inverse(&e, &phi);
 
     RsaKey { e, d, n }
+}
+
+// Euler's totient function φ(n)
+fn totient(p: HalfSizeBigUint, q: HalfSizeBigUint) -> BigUint {
+    let p_sub_1 = p.checked_sub(&HalfSizeBigUint::ONE).unwrap();
+    let q_sub_1 = q.checked_sub(&HalfSizeBigUint::ONE).unwrap();
+    p_sub_1 * q_sub_1
+}
+
+fn generate_random_prime<R: CryptoRngCore>(rng: &mut R) -> HalfSizeBigUint {
+    generate_prime_with_rng(rng, Some(HalfSizeBigUint::BITS))
+}
+
+#[inline]
+fn modulo(a: &BigUint, b: &BigUint) -> BigUint {
+    a.div_rem(&NonZero::new(*b).unwrap()).1
+}
+
+#[inline]
+fn divide(a: &BigUint, b: &BigUint) -> BigUint {
+    a.div_rem(&NonZero::new(*b).unwrap()).0
+}
+
+fn gcd(mut a: BigUint, mut b: BigUint) -> BigUint {
+    while b != BigUint::ZERO {
+        let temp = b;
+        b = modulo(&a, &b);
+        a = temp;
+    }
+    a
+}
+
+fn mod_inverse(a: &BigUint, m: &BigUint) -> BigUint {
+    let mut d = BigUint::from_u64(2);
+    // while (d * a).div_rem(&NonZero::new(*m).unwrap()).1 != BigUint::ONE {
+    //     d += BigUint::ONE;
+    // }
+    d
+}
+
+fn mod_pow(base: BigUint, exponent: BigUint, modulus: BigUint) -> BigUint {
+    let non_zero_modulus = NonZero::new(modulus).unwrap();
+    let params = DynResidueParams::new(&non_zero_modulus);
+    let residue_base = DynResidue::new(&base, params);
+    let result = residue_base.pow(&exponent).retrieve();
+    result
 }
